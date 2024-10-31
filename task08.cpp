03ff0b6c6ee212bcfbef75e613cde6f488a9a7ce
shakil <shakilmultan@gmail.com>
2024-10-31 22:47:20 +0500

    first repo

---
+++ task08.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while(i<=3){
+        cout<<"weak"<<i<<endl;
+        int j=1;
+        while(j<=7){
+            cout<<"day "<<j<<endl;
+            j++;
+        }
+        i++;
+
+        
+    }
+    return 0;
+}
